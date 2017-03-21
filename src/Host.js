74508fb2c13375af389455f96748a8908ac28b5d
Murray Smith <msmith@smarttech.com>
2017-03-21 09:16:30 -0600

    audio code cleanup

--- src/Host.js
+++ src/Host.js
@@ -17,10 +17,11 @@ class Host extends Component {
     players: undefined,
 
     audio: {
-      time: new Audio('/audio/time.wav'),
-      'daily-double': new Audio('/audio/daily-double.wav'),
       boardfill: new Audio('/audio/boardfill.wav'),
-      'chime': new Audio('/audio/chime.wav'),
+      'daily-double': new Audio('/audio/daily-double.wav'),
+      time: new Audio('/audio/time.wav'),
+      round: new Audio('/audio/round.wav'),
+      chime: new Audio('/audio/chime.wav'),
     },
   }
 
@@ -628,3 +629,35 @@ Host.defaultProps = {
 };
 
 export default Host;
+
+/*
+good
+X is right
+X is right, yes
+no
+and that would be X
+that's right
+X, that's right
+that correct response this time, is X
+Answer, daily double
+okay, # it is
+that's it
+right
+it's X
+yup
+you got him
+what is X
+that's followed by
+and finally
+he's the one
+and that is X
+correct
+looking for X
+that's right
+X, good
+that's the X
+X, right
+X, right you are
+that'd be X
+what is X
+// */
